3ac27814cba244a6dce2de365bceda81428c9a45
klosinm <monicaklosin@gmail.com>
2020-10-19 00:24:30 -0400

    added Portfolio

--- candypage.py
+++ candypage.py
@@ -1,18 +1,29 @@
 import flask as fl
-from flask import redirect, url_for,  render_template
+from flask import redirect, url_for, render_template
+
 
 
 app = fl.Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def home():
     return render_template("index.html")
+
+@app.route('/aboutMe')
+def aboutMe():
+    return render_template("aboutMe.html")
 
 
 @app.route('/resume')
 def resume():
     return render_template("resume.html")
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
+
 
 
 @app.route('/candyCharts')
@@ -29,3 +40,5 @@ def user(name):
 if __name__ == "__main__":
     #app.run('0.0.0.0', 9001)
     app.run(debug=True)
+
+
