d90883318574e9a27fb8d59c4c195df5f43d1879
klosinm <monicaklosin@gmail.com>
2020-10-18 22:53:57 -0400

    working on resume page

--- candypage.py
+++ candypage.py
@@ -1,12 +1,31 @@
 import flask as fl
+from flask import redirect, url_for,  render_template
+
 
 app = fl.Flask(__name__)
 
 
 @app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+
+@app.route('/candyCharts')
 def index():
     return fl.render_template('candy-name.html')
 
 
+@app.route("/<name>")
+def user(name):
+    # {redirect(url_for("home"))}"
+    return f"Whoops! {name} is not a valid page. Click here to go back home "
+
+
 if __name__ == "__main__":
-    app.run('0.0.0.0', 9001)
+    #app.run('0.0.0.0', 9001)
+    app.run(debug=True)
